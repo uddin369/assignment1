c07834a075e45a294aab7afaafacd209e9144cd7
Asad <uddin369@gmail.com>
2018-07-22 21:16:21 -0700

    assignment1

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 17:55:13 2018
+
+@author: HP
+"""
+from message import Message
+
+foo = Message('Hello World');
+print(foo.show());
+  
+ 
---
+++ message.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 22 18:08:03 2018
+
+@author: HP
+"""
+
+class Message:
+    def __init__(self,text):
+        self.note = text
+    def show(self):
+        return self.note;
